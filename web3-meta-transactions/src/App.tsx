4f6a71db4010461a9e52af4fc0e1b87661d0b7f8
rymksontini <rymksontini@gmail.com>
2021-03-15 14:08:38 +0100

    example usecase metamask account with no balance

--- web3-meta-transactions/src/App.tsx
+++ web3-meta-transactions/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useContext, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import {MetaTx} from "./components/MetaTx";
 import {Wallet} from "@ethersproject/wallet"
 import useEthSWR from "ether-swr";
@@ -7,6 +7,7 @@ import {useWeb3React} from "@web3-react/core";
 import {Contract} from "@ethersproject/contracts";
 import {additionContract, setAContract} from "./utils";
 import {ExecuteMetaTx} from "./components/ExecuteMetaTx";
+import {TransactionResponse} from "@ethersproject/providers";
 
 require("dotenv").config();
 
@@ -14,22 +15,10 @@ require("dotenv").config();
 
 export const GetA = () => {
     const [res, setRes] = useState("");
-    const {library} = useWeb3React();
-    const wallet = new Wallet(process.env.REACT_APP_rinkeby_key as string,library)
-
-    // interacting with smart contract using ethers
-    const contract = new Contract(setAContract.address,setAContract.abi,wallet);
-    // const getA = async (contract: any) => {
-    //     const tx = await execute(contract.getA);
-    //     console.log(tx);
-    //     setRes(tx);
-    //     console.log(tx);
-    // };
-    // React.useEffect(() => {
-    //     getA(contract);
-    // }, [contract]);
-
-
+    const [clicked, setClicked] = useState(false);
+    const activate = () =>{
+        setClicked(true);
+    }
     // works only when metamask is activated (injected connector)
     //  const { data: res, error} = useEthSWR(
     //      [additionContract.address, 'doAddition',1,2]
@@ -40,17 +29,64 @@ export const GetA = () => {
     //  }
     // if (res){
     return (
-        <ExecuteMetaTx method={contract.getA} result={setRes}>
-            <div>A: {res.toString()} </div>
-        </ExecuteMetaTx>
+        <div>
+            <button onClick={activate}> Get A </button>
+            {(clicked &&
+                <ExecuteMetaTx contract={setAContract} method={"getA"} result={setRes}>
+                    <div>A: {res.toString()} </div>
+                </ExecuteMetaTx>)}
+        </div>
     )
     // }
     // return <></>
 }
+
+export const SetA = () => {
+    const [res, setRes] = useState(null);
+    const [input, setInput] = useState(-1);
+    const [txDone, setTxDone] = useState(false);
+    const [clicked, setClicked] = useState(false);
+    const activate = () =>{
+        setClicked(true);
+    }
+
+    const handleChange = (event: any) => {
+        console.log(event.target.value)
+        setInput(+event.target.value)
+    }
+
+    const waitTx = async () => {
+        // @ts-ignore
+        await res.wait();
+        setTxDone(true);
+    }
+
+    React.useEffect(() => {
+        console.log("txDone", txDone);
+        if(res && clicked){
+            console.log("res Tx", res);
+            waitTx();
+        }
+    },[res, clicked]);
+    return (
+        <div>
+            <input type="number" onChange={handleChange}/>
+            <button onClick={activate}> Set A </button>
+            {(clicked &&
+                <ExecuteMetaTx contract={setAContract} params={[input]} method={"setA"} result={setRes}/>)}
+            {(txDone &&
+                <div>Transaction successfully executed</div>)}
+        </div>
+    )
+    // }
+    // return <></>
+}
+
 export const App = () => {
     return (
         <MetaTx contracts={[additionContract, setAContract]}>
             <GetA/>
+            <SetA/>
         </MetaTx>
     )
 }
--- web3-meta-transactions/src/components/ExecuteMetaTx.tsx
+++ web3-meta-transactions/src/components/ExecuteMetaTx.tsx
@@ -4,26 +4,32 @@ import {execute} from "../utils";
 import {useWeb3React} from "@web3-react/core";
 import {Wallet} from "@ethersproject/wallet";
 import {InfuraProvider} from "@ethersproject/providers";
+import {Contract} from "@ethersproject/contracts";
 
 export const ExecuteMetaTx = (props: any) => {
     const {library, account} = useWeb3React();
+    // interacting with smart contract using ethers
     const provider = new  InfuraProvider("rinkeby", process.env.REACT_APP_RPC_API_KEY_4 as string)
     console.log("provider", provider);
     const wallet = new Wallet(process.env.REACT_APP_rinkeby_key as string, provider)
     console.log("wallet", wallet);
+    const contract = new Contract(props.contract.address, props.contract.abi, wallet);
     // const {signingAccount} = useContext(MetaTxContext);
     // console.log("signingAccount")
     // console.log(signingAccount);
-    const getA = async () => {
+    const executeTx = async () => {
         if (account){
-            const tx = await execute(account, library, wallet, props.method, props.params);
+            const tx = await execute(account, library, wallet, contract[props.method], props.params);
             console.log(tx);
             props.result(tx);
             console.log(tx);
         }
     };
     React.useEffect(() => {
-        getA();
+        executeTx();
     },[]);
-    return props.children
+    return (props.children
+        ? props.children
+        : <></>
+    )
 }
